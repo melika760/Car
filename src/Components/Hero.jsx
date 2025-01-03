0e01a0e67bf3d84dcfd32ad7cf70a7cef4a9f95e
melika760 <melikaaatafazoli@gmail.com>
2025-01-03 17:10:03 +0000

    working on car

---
+++ src/Components/Hero.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <section className='flex justify-between m-auto'>
+<div className=''>
+<h1 className='text-4xl font-bold'>Premium Car Rental in London</h1>
+<p className='text-2xl text-gray-800'>Don't deny yourself the pleasure of driving  the best permium cars from around the world here and now.</p>
+</div>
+<div>
+    images
+</div>
+    </section>
+  )
+}
+
+export default Hero
