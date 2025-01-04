43d5891f360ba1ed2e9aefdcacf87de1bf4f4ddf
melika760 <melikaaatafazoli@gmail.com>
2025-01-04 14:55:54 +0000

    working on nav

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -3,13 +3,13 @@ import porshce from '../assets/Porsche.png'
 const Hero = () => {
   return (
     <section className='flex justify-between m-[6rem] align-center items-center'>
-<div className=''>
-<h1 className='text-6xl font-bold font-[Poppins] mb-4 text-red-800'>Premium Car Rental in London</h1>
-<p className='text-xl text-gray-700 font-[Poppins]'>Don't deny yourself the pleasure of driving  the best permium cars from around the world here and now.</p>
-</div>
-<div className='w-[90%]'>
+    <div className=''>
+    <h1 className='text-6xl font-bold font-[Poppins] mb-4 '>Premium Car Rental in London</h1>
+    <p className='text-xl text-gray-700 font-[Poppins]'>Don't deny yourself the pleasure of driving  the best permium cars from around the world here and now.</p>
+    </div>
+    <div className='w-[90%]'>
     <img src={porshce} className='w-full object-contain' alt='Porsche'/>
-</div>
+    </div>
     </section>
   )
 }
--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
-
+import logo from '../assets/logo.png'
 const Nav = () => {
+  const menu=[{
+    name:"Cars",
+    id:1,
+    path:'#Cars'
+  },
+  {
+    name:"About Us",
+    id:2,
+    path:'#About'
+  },
+  {
+    name:"Testemonial",
+    id:3,
+    path:'#Testemonial'
+  },
+]
   return (
+  <nav className='flex m-3 justify-between items-center'>
     <div>
-      hi
+    <img src={logo} alt='logo' className='w-[100px] h-[100px]'/>
     </div>
+  <ul className='flex space-x-9 items-center'>
+{menu.map((item)=>(<li key={item.id} className='cursor-pointer'><a href={item.path} alt={item.name} className='text-lg hover:text-red-700'>{item.name}</a></li>))}
+  {/* reactrouter needed */}
+  <button className='text-red-700 border-red-700 border w-[100px] rounded-xl p-2 hover:bg-red-700 hover:text-white'><a href='/login' alt='login'>Login</a></button>
+  </ul>
+  </nav>
   )
 }
 
