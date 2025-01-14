c1165968df94a9a43dd0eab423b6ee5ad66e1dbe
melika760 <melikaaatafazoli@gmail.com>
2025-01-14 13:31:05 +0000

    make hero responsive

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import porshce from '../assets/Porsche.png'
 const Hero = () => {
   return (
-    <section className='flex justify-between m-[6rem] align-center items-center'>
+    <section className='flex justify-between m-[6rem] align-center items-center max-md:flex-col'>
     <div className=''>
-    <h1 className='text-6xl font-bold font-[Poppins] mb-4 '>Premium Car Rental in London</h1>
-    <p className='text-xl text-gray-700 font-[Poppins]'>Don't deny yourself the pleasure of driving  the best permium cars from around the world here and now.</p>
+    <h1 className='text-6xl font-bold font-[Poppins] mb-4  max-md:text-center'>Premium Car Rental in London</h1>
+    <p className='text-xl text-gray-700 font-[Poppins] max-md:text-center'>Don't deny yourself the pleasure of driving  the best permium cars from around the world here and now.</p>
     </div>
     <div className='w-[90%]'>
         <img src={porshce} className='w-full object-contain scale-110' alt='Porsche'/>
--- src/Components/Logos.jsx
+++ src/Components/Logos.jsx
@@ -3,10 +3,10 @@ import { Carlogos } from './Constants'
 
 const Logos = () => {
   return (
-  <section className='m-[5rem] grid grid-cols-6 gap-7'>
- {Carlogos.map((item)=>(   <div key={item.make} className='w-[220px] h-[120px] border border-gray-200 shadow-md rounded-md flex flex-col justify-center items-center p-2 hover:bg-gray-200 hover:shadow-2xl cursor-pointer'>
+  <section className='m-[5rem] grid grid-cols-6 gap-20 max-md:grid-cols-3'>
+ {Carlogos.map((item)=>(<div key={item.make} className='w-[220px] h-[120px] max-md:w-[90%] border border-gray-200 shadow-md rounded-md flex flex-col justify-center items-center p-2 hover:bg-gray-200 hover:shadow-2xl cursor-pointer'>
     <div>
-    <img src={item.img} alt={item.make} className='object-contain w-full h-[105px]'/>
+    <img src={item.img} alt={item.make} className='object-contain w-full h-[105px] max-md:h-[80px]'/>
     </div>
  {/* <p className='text=gray-500 text-lg'>{item.make}</p> */}
   </div>)
