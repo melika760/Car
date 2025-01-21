75604af0fd39a798a915ee1f023df7bd00550724
melika760 <melikaaatafazoli@gmail.com>
2025-01-21 14:28:08 +0000

    working on Card section

--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -5,14 +5,27 @@ import gas from "../assets/gas.png";
 import Wheel from "../assets/steering-wheel.png";
 const Cards = () => {
   return (
-    <div className='w-[40%] border border-gray-300 shadow-sm flex flex-col  rounded-md p-4 space-y-10'>
-      <div className='w-[80%] m-auto items-center'>
+    <div className='border border-gray-100 shadow-sm flex flex-col  rounded-lg p-4 space-y-10 bg-gray-100 relative hover:bg-white'>
+      <h1 className='text-2xl font-semibold '>Toyota Camery</h1>
+      <p className='flex mt-6 text-[32px] font-extrabold font-[Poppins]'>
+    <span className='self-start text-[14px] font-semibold'>$</span>22
+    <span className='self-end text-[14px] font-medium'>/day</span>
+</p>
+      <div className=' m-auto items-center'>
 <img src={carimg} alt='porsche' className='w-full object-contain'/>
       </div>
-      <div className='flex justify-between m-3'>
-     <div className='flex flex-col justify-center items-center'>
-      <img src={Wheel} alt='' className='w-[23px]'/>
-      <p className='font-[Poppins]'>Manual</p>
+      <div className='flex justify-between m-3 group-hover:invisible'>
+     <div className='flex flex-col space-y-3 m-2  items-center'>
+      <img src={Wheel} alt='Steering Wheel' className='w-[23px]'/>
+      <p className='font-[Poppins] text-gray'>Manual</p>
+     </div>
+     <div className='flex flex-col space-y-3 m-2  items-center'>
+      <img src={tire} alt='Tire' className='w-[23px]'/>
+      <p className='font-[Poppins] text-gray'>FWD</p>
+     </div>
+     <div className='flex flex-col space-y-3 m-2  items-center'>
+      <img src={gas} alt='Fuel' className='w-[23px]'/>
+      <p className='font-[Poppins] text-gray'>Gas</p>
      </div>
       </div>
 
